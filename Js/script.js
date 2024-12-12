5e4dfeb9d0168202ce1ea56e6088226a4a213cb1
Arthur <75622979+800Arthur008@users.noreply.github.com>
2024-12-11 23:02:30 -0300

    soma

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,10 @@
-let fundo = document.body
-fundo.style.backgroundColor = 'yellow'
+let tn1 = document.getElementById('tn1');
+let tn2 = document.getElementById('tn2');
+let res = document.getElementById('res');
 
+function soma() {
+    let n1 = Number(tn1.value);
+    let n2 = Number(tn2.value);
+    let s = n1 + n2;
+    res.innerHTML = `O valor da soma de ${n1} e ${n2} é: ${s}` 
+}
