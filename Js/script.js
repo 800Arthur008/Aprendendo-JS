2459fccc09d1bcdc284afa5171c1ded166ed5885
Arthur <75622979+800Arthur008@users.noreply.github.com>
2024-12-13 18:40:17 -0300

    versao final

--- Js/script.js
+++ Js/script.js
@@ -15,6 +15,11 @@ function operador(op) {
         sub.style.color = 'black'
         mult.style.color = 'black'
         divi.style.color = 'black'
+
+        soma.style.fontSize = '24px'
+        sub.style.fontSize= '15px'
+        mult.style.fontSize = '15px'
+        divi.style.fontSize = '15px'
     }
     if (op === '-'){
         operando = 2
@@ -22,6 +27,11 @@ function operador(op) {
         sub.style.color = 'red'
         mult.style.color = 'black'
         divi.style.color = 'black'
+
+        soma.style.fontSize = '15px'
+        sub.style.fontSize= '24px'
+        mult.style.fontSize = '15px'
+        divi.style.fontSize = '15px'
     }
     else if (op === '*'){
         operando = 3
@@ -29,6 +39,11 @@ function operador(op) {
         sub.style.color = 'black'
         mult.style.color = 'red'
         divi.style.color = 'black'
+
+        soma.style.fontSize = '15px'
+        sub.style.fontSize= '15px'
+        mult.style.fontSize = '24px'
+        divi.style.fontSize = '15px'
     }
     else if(op === '/') {
         operando = 4
@@ -36,6 +51,27 @@ function operador(op) {
         sub.style.color = 'black'
         mult.style.color = 'black'
         divi.style.color = 'red'
+
+        soma.style.fontSize = '15px'
+        sub.style.fontSize= '15px'
+        mult.style.fontSize = '15px'
+        divi.style.fontSize = '24px'
+    }
+
+    else if(op === '') {
+        operando = 4
+        soma.style.color = 'black'
+        sub.style.color = 'black'
+        mult.style.color = 'black'
+        divi.style.color = 'black'
+
+        soma.style.fontSize = '15px'
+        sub.style.fontSize= '15px'
+        mult.style.fontSize = '15px'
+        divi.style.fontSize = '15px'
+
+        tn1.value = ''
+        tn2.value = ''
     }
 }
 
@@ -44,21 +80,21 @@ function calc() {
     let n2 = Number(tn2.value);
     if (operando == 1) {
         let r = n1 + n2
-        res.innerHTML = `O valor da soma de ${n1} e ${n2} é: ${r}`
+        res.innerHTML = `O valor da soma entre ${n1} e ${n2} é: ${r}`
     }
     else if (operando == 2) {
         let r = n1 - n2
-        res.innerHTML = `O valor da subtração de ${n1} e ${n2} é: ${r}`
+        res.innerHTML = `O valor da subtração entre ${n1} e ${n2} é: ${r}`
     }
     else if (operando == 3) {
         let r = n1 * n2
-        res.innerHTML = `O valor da multiplicação de ${n1} e ${n2} é: ${r}`
+        res.innerHTML = `O valor da multiplicação entre ${n1} e ${n2} é: ${r}`
     }
     else if (operando == 4) {
-        let r = n1 - n2
-        res.innerHTML = `O valor da divisão de ${n1} e ${n2} é: ${r}`
+        let r = n1 / n2
+        res.innerHTML = `O valor da divisão entre ${n1} e ${n2} é: ${r}`
     }
     else{
-        res.innerHTML = `Por favor selecione um operador`
+        res.innerHTML = `Por favor selecione um operador!`
     }
 }
